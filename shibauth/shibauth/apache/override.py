d3ef9cce0ab63220b7b43dda04554273d4c55664
sheha <i.sheeha@gmail.com>
2018-01-06 01:59:08 +0100

    Shibboleth Auth: Override

--- shibauth/shibauth/apache/override.py
+++ shibauth/shibauth/apache/override.py
@@ -1,4 +1,4 @@
 from shibauth.shibauth.settings import *
 
 DEBUG = True
-ALLOWED_HOSTS = ["pathfinder.dev", "127.0.0.1", "127.0.1.1"]
+ALLOWED_HOSTS = ["pathfinder-shibbauth", "127.0.0.1", "127.0.1.1"]
