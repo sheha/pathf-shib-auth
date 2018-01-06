366fe8b631f11cbce9f66a1ae4941124dc2d9518
sheha <i.sheeha@gmail.com>
2018-01-06 02:22:27 +0100

    Shibboleth Auth: More Cleanup

--- shibauth/shibauth/urls.py
+++ shibauth/shibauth/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views
-from auth.forms import LoginForm
+from shibauth.auth.forms import LoginForm
 
 urlpatterns = [
     url(r'^admin/',include(admin.site.urls)),
